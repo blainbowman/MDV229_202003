769b40c8468ca70409695001a2dde690cc35400c
Blain <bbowman@student.fullsail.edu>
2020-03-06 15:50:53 -0800

    Starting convert data to json

---
+++ BowmanBlain_ConvertedData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BowmanBlain_ConvertedData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
