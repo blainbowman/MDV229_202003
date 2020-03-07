85d516fbf897cd32175b2c228c0f7d5cb7324625
Blain <bbowman@student.fullsail.edu>
2020-03-06 16:00:42 -0800

    Added mysql nuget to converted data project and created a class called Restaurantprofiles

--- BowmanBlain_ConvertedData/Program.cs
+++ BowmanBlain_ConvertedData/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using MySql.Data.MySqlClient;
 
 namespace BowmanBlain_ConvertedData
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
---
+++ BowmanBlain_ConvertedData/Restaurantprofiles.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowmanBlain_ConvertedData
+{
+    class Restaurantprofiles
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Time { get; set; }
+        public string Price { get; set; }
+        public string Location { get; set; }
+        public string Cuisine { get; set; }
+        public float FoodRating { get; set; }
+        public float ServiceRating { get; set; }
+        public float AmbienceRating { get; set; }
+        public float ValueRating { get; set; }
+        public float OverallRating { get; set; }
+        public float OverallPossible { get; set; }
+        public override string ToString()
+        {
+            return Name.ToString() + ", " + Address.ToString() + ",  " + Phone.ToString() + ",  " + Time.ToString() + ",  " + Price.ToString() + ",  " + Location.ToString() + ",  " + Cuisine.ToString() + ",  " + FoodRating.ToString() + ",  " + ServiceRating.ToString() + ",  " + AmbienceRating.ToString() + ",  " + ValueRating.ToString() + ",  " + OverallRating.ToString() + ",  " + OverallPossible.ToString();
+        }
+    }
+}
