01927f9d1d1b59e57f90485f3cf95ae8c9f258f5
Blain <bbowman@student.fullsail.edu>
2020-03-11 15:11:27 -0700

    Convert Data Project - rewrote some of the sql to json to work with the new database class

--- BowmanBlain_ConvertedData/Program.cs
+++ BowmanBlain_ConvertedData/Program.cs
@@ -81,68 +81,44 @@ namespace BowmanBlain_ConvertedData
         public static void SqlToJson()
         {
 
-            DataConn();
-
-            Console.WriteLine("File has been created in " + _directory + "BowmanBlain_ConvertedDate.json");
-        }
-
-        public static void DataConn()
-        {
-
-            try
+            Restaurants_DB rest = new Restaurants_DB(); //class instance creation 
+            rest.restaurantprofiles = new List<Restaurantprofiles>(); //create sheet instance
+            DataBase db1 = new DataBase();  //class instance creation 
+            db1.OpenConnection();              //connect to database
+            MySqlDataReader reader = db1.DataReader("SELECT RestaurantName, IFNULL(Address, 0), IFNULL(Phone, 0), IFNULL(HoursOfOperation, 0), IFNULL(Price, 0), USACityLocation, Cuisine,  IFNULL(FoodRating, 0), IFNULL(ServiceRating, 0), IFNULL(AmbienceRating, 0), IFNULL(ValueRating, 0), IFNULL(OverallRating, 0), IFNULL(OverallPossibleRating, 0) FROM restaurantprofiles;");
+            while (reader.Read())              //taking data from database
             {
 
-                string connStr = @"server= 127.0.0.1;userid=root;password=root;database=SampleRestaurantDatabase;port=8889";
-                MySqlConnection conn = new MySqlConnection(connStr);
-                conn.Open();
-                string sql = "SELECT RestaurantName, IFNULL(Address, 0), IFNULL(Phone, 0), IFNULL(HoursOfOperation, 0), IFNULL(Price, 0), USACityLocation, Cuisine,  IFNULL(FoodRating, 0), IFNULL(ServiceRating, 0), IFNULL(AmbienceRating, 0), IFNULL(ValueRating, 0), IFNULL(OverallRating, 0), IFNULL(OverallPossibleRating, 0) FROM restaurantprofiles;";
-                MySqlCommand command = new MySqlCommand(sql, conn);
-                MySqlDataReader reader = command.ExecuteReader();
-                Restaurants_DB rest = new Restaurants_DB();
-                rest.restaurantprofiles = new List<Restaurantprofiles>();
+                Restaurantprofiles restaurants_list = new Restaurantprofiles(); //class instance creation 
 
-
-                while (reader.Read())
-                {
-
-                    Restaurantprofiles restaurants_list = new Restaurantprofiles();
-
-                    restaurants_list.Name = reader.GetString(0);
-                    restaurants_list.Address = reader.GetString(1);
-                    restaurants_list.Phone = reader.GetString(2);
-                    restaurants_list.Time = reader.GetString(3);
-                    restaurants_list.Price = reader.GetString(4);
-                    restaurants_list.Location = reader.GetString(5);
-                    restaurants_list.Cuisine = reader.GetString(6);
-                    restaurants_list.FoodRating = float.Parse(reader.GetString(7));
-                    restaurants_list.ServiceRating = float.Parse(reader.GetString(8));
-                    restaurants_list.AmbienceRating = float.Parse(reader.GetString(9));
-                    restaurants_list.ValueRating = float.Parse(reader.GetString(10));
-                    restaurants_list.OverallRating = float.Parse(reader.GetString(11));
-                    restaurants_list.OverallPossible = float.Parse(reader.GetString(12));
-                    rest.restaurantprofiles.Add(restaurants_list);
-                }
-
-                reader.Close();
-                conn.Close();
-                string serialized = JsonConvert.SerializeObject(rest, Formatting.Indented);
-                string a = serialized;
-                if (!Directory.Exists(_directory)) Directory.CreateDirectory(_directory);
-                string writePath = _directory + "BowmanBlain_ConvertedDate.json";
-                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
-                {
-                    sw.WriteLine(a);
-                }
-
-
+                restaurants_list.Name = reader.GetString(0);
+                restaurants_list.Address = reader.GetString(1);
+                restaurants_list.Phone = reader.GetString(2);
+                restaurants_list.Time = reader.GetString(3);
+                restaurants_list.Price = reader.GetString(4);
+                restaurants_list.Location = reader.GetString(5);
+                restaurants_list.Cuisine = reader.GetString(6);
+                restaurants_list.FoodRating = float.Parse(reader.GetString(7));
+                restaurants_list.ServiceRating = float.Parse(reader.GetString(8));
+                restaurants_list.AmbienceRating = float.Parse(reader.GetString(9));
+                restaurants_list.ValueRating = float.Parse(reader.GetString(10));
+                restaurants_list.OverallRating = float.Parse(reader.GetString(11));
+                restaurants_list.OverallPossible = float.Parse(reader.GetString(12));
+                rest.restaurantprofiles.Add(restaurants_list);
             }
-            catch (MySqlException e)
+
+            reader.Close(); //close reading
+            db1.CloseConnection();                    //close connect to database
+            string serialized = JsonConvert.SerializeObject(rest, Formatting.Indented); //serialize data to json
+            string a = serialized;
+            if (!Directory.Exists(_directory)) Directory.CreateDirectory(_directory);   //check and create folder
+
+            string writePath = _directory + "BowmanBlain_ConvertedDate.json";               //route to file
+            using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))           //writing into the file
             {
-                Console.WriteLine("Error:{0}", e.ToString());
-                Console.WriteLine("Press any key to continue...");
-                Console.ReadKey();
-                Environment.Exit(0);
+                sw.WriteLine(a);
             }
+            Console.WriteLine("File has created in " + _directory + "BowmanBlain_ConvertedDate.json");            //print info about saving file
         }
     }
 }
