3adf1053c45a118b813183aa73aaa7a9b217052e
Blain <bbowman@student.fullsail.edu>
2020-03-25 16:39:21 -0700

    Converted Data Project - finished user class with getters and setters and formating

--- BowmanBlain_ConvertedData/User.cs
+++ BowmanBlain_ConvertedData/User.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace BowmanBlain_ConvertedData
 {
     class User
     {
+        public int player { get; set; } //number of player
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+
+        public int total { get; set; } //Score
+
+        public override string ToString() //return result in the format
+        {
+            return "Player " + player + " - " + first_name + " " + last_name;
+        }
+        public string ToString1() //return result in the format
+        {
+            return "Score: " + total;
+        }
     }
+
 }
