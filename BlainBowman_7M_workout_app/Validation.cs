1a912e047c2c8a2771d3b87d7d18a1768a8e045d
Blain <bbowman@student.fullsail.edu>
2020-03-25 18:44:48 -0700

    Custom App Project - added some validation for better user experience

--- BlainBowman_7M_workout_app/Validation.cs
+++ BlainBowman_7M_workout_app/Validation.cs
@@ -8,5 +8,33 @@ namespace BlainBowman_7M_workout_app
 {
     class Validation
     {
+        public static string ValidateString(string s)
+        {
+            Console.Write(s);
+            string response = Console.ReadLine();
+            while (string.IsNullOrWhiteSpace(response))
+            {
+                Console.Write("\rPlease do not leave this empty. Try again: ");
+                response = Console.ReadLine();
+            }
+            return response;
+        }
+
+        public static int GetInt(int min, int max, string message = "Enter an integer: ")
+        {
+            int validatedInt;
+            string input = null;
+            do
+            {
+                Console.Write(message);
+                input = Console.ReadLine();
+                if (!(Int32.TryParse(input, out validatedInt))) { Console.WriteLine("Not an Integer. Try again..."); continue; }
+                if (!(validatedInt >= min && validatedInt <= max)) { Console.WriteLine("Out of Range ({0}-{1}). Try again...", min, max); }
+            } while (!(Int32.TryParse(input, out validatedInt) && (validatedInt >= min && validatedInt <= max)));
+
+
+            return validatedInt;
+        }
+
     }
 }
