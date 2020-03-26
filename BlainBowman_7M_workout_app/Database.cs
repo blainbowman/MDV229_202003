fecbfad17b85a65cf06c2632f8b0e1e7e02c200f
Blain <bbowman@student.fullsail.edu>
2020-03-25 18:26:02 -0700

    Custom App Project - coded the database class with a connect string as well as a function to close the connection

--- BlainBowman_7M_workout_app/Database.cs
+++ BlainBowman_7M_workout_app/Database.cs
@@ -8,5 +8,50 @@ namespace BlainBowman_7M_workout_app
 {
     class Database
     {
+        private Database()
+        {
+
+        }
+
+        private string databaseName = string.Empty;
+        public string DatabaseName
+        {
+            get { return databaseName; }
+            set { databaseName = value; }
+        }
+
+        public string Password { get; set; }
+        private MySqlConnection connection = null;
+        public MySqlConnection Connection
+        {
+            get { return connection; }
+        }
+
+        private static Database _instance = null;
+        public static Database Instance()
+        {
+            if (_instance == null)
+                _instance = new Database();
+            return _instance;
+        }
+
+        public bool IsConnect()
+        {
+            if (Connection == null)
+            {
+                if (String.IsNullOrEmpty(databaseName))
+                    return false;
+                string connstring = string.Format("Server=localhost; port=8889; UID=root; password=root; database ={0}", databaseName);
+                connection = new MySqlConnection(connstring);
+                connection.Open();
+            }
+
+            return true;
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
     }
 }
