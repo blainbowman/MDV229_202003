707d06355a9458a10c04d2ad28143b87dd027210
Blain <bbowman@student.fullsail.edu>
2020-03-25 18:30:22 -0700

    Custom App Project - added a login menu when you first start the app

--- BlainBowman_7M_workout_app/Menu.cs
+++ BlainBowman_7M_workout_app/Menu.cs
@@ -8,5 +8,34 @@ namespace BlainBowman_7M_workout_app
 {
     class Menu
     {
+        private static Database dbCon = Database.Instance();
+        private static string currentUser = null;
+        private static int currentUserId = 0;
+        public static void SignUpAndLogInMenu()
+        {
+            Console.Clear();
+            dbCon.DatabaseName = "Blain_Bowman_7M_Workout_App";
+            Console.WriteLine("Welcome!!!");
+            Console.WriteLine("Let's get started.");
+            Console.WriteLine("What do you want to do? ");
+            Console.WriteLine("\t[1] Log in");
+            Console.WriteLine("\t[2] Sign up");
+            Console.WriteLine("\t[3] Exit");
+            int userOptionNumber = Validation.GetInt(1, 3, "Please, choose an option: ");
+            switch (userOptionNumber)
+            {
+                case 1:
+                    LogIn();
+                    break;
+                case 2:
+                    SignUp();
+                    break;
+                case 3:
+                    dbCon.Close();
+                    Environment.Exit(1);
+                    break;
+            }
+            MainMenu();
+        }
     }
 }
