d9bbcde3dd214afd46110b1d17f89821ce6f88c7
Blain <bbowman@student.fullsail.edu>
2020-03-27 11:26:14 -0700

    Time Tracker App - Added database class with precoded code

---
+++ BlainBowman_TimeTrackerApp/Database.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace BlainBowman_TimeTrackerApp
+{
+    class Database
+    {
+        private Database()
+        {
+
+        }
+
+        private string databaseName = string.Empty;
+        public string DatabaseName
+        {
+            get { return databaseName; }
+            set { databaseName = value; }
+        }
+
+        public string Password { get; set; }
+        private MySqlConnection connection = null;
+        public MySqlConnection Connection
+        {
+            get { return connection; }
+        }
+
+        private static Database _instance = null;
+        public static Database Instance()
+        {
+            if (_instance == null)
+                _instance = new Database();
+            return _instance;
+        }
+
+        public bool IsConnect()
+        {
+            if (Connection == null)
+            {
+                if (String.IsNullOrEmpty(databaseName))
+                    return false;
+                string connstring = string.Format("Server=localhost; port=8889; UID=root; password=root; database ={0}", databaseName);
+                connection = new MySqlConnection(connstring);
+                connection.Open();
+            }
+
+            return true;
+        }
+
+        public void Close()
+        {
+            connection.Close();
+            
+        }
+    }
+}
