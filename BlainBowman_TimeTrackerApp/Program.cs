a64879cc82549bbb0d8b9c597c836ca24e7d826a
Blain <bbowman@student.fullsail.edu>
2020-03-27 11:20:41 -0700

    Time Tracker App - Start

---
+++ BlainBowman_TimeTrackerApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlainBowman_TimeTrackerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
