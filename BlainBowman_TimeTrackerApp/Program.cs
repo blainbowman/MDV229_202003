eb261875fe78771ef63526f5e8888ca12e014fc8
Blain <bbowman@student.fullsail.edu>
2020-03-27 11:30:22 -0700

    Time Tracker App - Updated Program class and finished the app

---
+++ BlainBowman_TimeTrackerApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+namespace BlainBowman_TimeTrackerApp
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Menu.Login();
+            
+            Menu.MainMenu(Menu.name);
+            Console.ReadKey();
+        }
+    }
+}
